cf7085c056de13235fbbc1dcfdb48574b211ce01
Ayush Shaw <ayushshaw127@gmail.com>
2022-08-15 18:52:45 +0530

    modified  navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ export default function navbar() {
     return (
         <header>
             <img src="logo.png" alt="Logo" />
-            <h1 class="brandName"><span>W</span>isata</h1>
+            <h1 className="brandName"><span>W</span>isata</h1>
 
-            <nav class="navbar">
+            <nav className="navbar">
                 <a href="#home">Home</a>
                 <a href="#packages">Packages</a>
                 <a href="#bookNow">Book Now</a>
@@ -15,15 +15,15 @@ export default function navbar() {
                 <a href="#contact">Contact</a>
 
             </nav>
-            <div class="icons">
-                <i class="fas fa-search" id="search-btn"></i>
-                <i class="fas fa-user" id="login-btn"></i>
+            <div className="icons">
+                <i className="fas fa-search" id="search-btn"></i>
+                <i className="fas fa-user" id="login-btn"></i>
 
             </div>
 
-            <form action="" class="search-bar-container">
-                <input type="search" class="" id="search-bar" placeholder="Search here" />
-                <label for="search-bar" class="fas fa-search"></label>
+            <form action="" className="search-bar-container">
+                <input type="search" className="" id="search-bar" placeholder="Search here" />
+                <label htmlFor="search-bar" className="fas fa-search"></label>
             </form>
         </header>
     )
