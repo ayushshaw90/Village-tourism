5f3266fab6b4ac2bfb1bffbf423deef923bee6d3
Ayush Shaw <ayushshaw127@gmail.com>
2022-08-15 17:04:28 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Mainform from './components/Mainform'
-import Villages from './components/Villages'
+import * as React from 'react'
+import Navbar from './components/Navbar'
 
-import Navbar from './components/Navbar';
-function App() {
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
+
+export default function App() {
+  // 2. Wrap ChakraProvider at the root of your app
   return (
-    <header>
-        <img src="logo.png" alt="Logo" />
-        <h1 class="brandName"><span>W</span>isata</h1>
-
-        <nav class="navbar">
-        <a href="#home">Home</a>
-        <a href="#packages">Packages</a>
-        <a href="#bookNow">Book Now</a>
-        <a href="#services">Services</a>
-        <a href="#review">Review</a>
-        <a href="#contact">Contact</a>
-
-        </nav>
-        <div class="icons">
-            <i class="fas fa-search" id="search-btn"></i>
-            <i class="fas fa-user" id="login-btn"></i>
-        
-        </div>
-    
-        <form action="" class="search-bar-container">
-            <input type="search" class="" id="search-bar" placeholder="Search here" />
-            <label for="search-bar" class="fas fa-search"></label>
-        </form>
-    </header>
-  );
+    <ChakraProvider>
+      <Navbar />
+    </ChakraProvider>
+  )
 }
-
-export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,30 @@
-export default function Navbar(){
-    return(
-        <nav class="navbar">
-        <a href="#home">Home</a>
-        <a href="#packages">Packages</a>
-        <a href="#bookNow">Book Now</a>
-        <a href="#services">Services</a>
-        <a href="#review">Review</a>
-        <a href="#contact">Contact</a>
+import "./Navbar.css"
 
-        </nav>
+export default function navbar() {
+    return (
+        <header>
+            <img src="logo.png" alt="Logo" />
+            <h1 class="brandName"><span>W</span>isata</h1>
+
+            <nav class="navbar">
+                <a href="#home">Home</a>
+                <a href="#packages">Packages</a>
+                <a href="#bookNow">Book Now</a>
+                <a href="#services">Services</a>
+                <a href="#review">Review</a>
+                <a href="#contact">Contact</a>
+
+            </nav>
+            <div class="icons">
+                <i class="fas fa-search" id="search-btn"></i>
+                <i class="fas fa-user" id="login-btn"></i>
+
+            </div>
+
+            <form action="" class="search-bar-container">
+                <input type="search" class="" id="search-bar" placeholder="Search here" />
+                <label for="search-bar" class="fas fa-search"></label>
+            </form>
+        </header>
     )
 }
